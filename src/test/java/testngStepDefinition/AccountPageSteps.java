59b382ddd782ace1a773027c2331b3d07f0f87a3
LOBO <LOBO@DESKTOP-UUPN511>
2022-07-19 13:26:09 +0530

    Next Checkin

--- src/test/java/testngStepDefinition/AccountPageSteps.java
+++ src/test/java/testngStepDefinition/AccountPageSteps.java
@@ -42,8 +42,7 @@ public class AccountPageSteps {
 	@Given("user is on Accounts Page")
 	public void user_is_on_accounts_page() {
 	   accountPage.getAccountsPageTitle();
-	   
-	   }   
+	   	  }   
 
 	@Then("user gets account section")
 	public void user_gets_account_section(DataTable sectionsTable) {
